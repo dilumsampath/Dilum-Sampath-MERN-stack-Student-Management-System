35cc3ca5f818a885b64ed769d8a2220b2ac64c53
dilumsampath <dilumsampathkoodagoda@gmail.com>
2024-06-16 23:33:50 +0530

    linked DataBase with backend serverand backend server is up and running

--- backEnd/server.js
+++ backEnd/server.js
@@ -4,10 +4,27 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8070;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-let wse;
+const URI = process.env.MONGODB_URI;
+
+mongoose.connect(URI, {
+  //   useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //   useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Mongodb Connection Succesful");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port number: ${PORT}`);
+});
