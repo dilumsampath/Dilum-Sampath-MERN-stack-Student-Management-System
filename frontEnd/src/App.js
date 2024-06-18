76fe5ab8885ef2a3f516c79b95b7572b3741bd97
dilumsampath <dilumsampathkoodagoda@gmail.com>
2024-06-19 01:11:52 +0530

    create header

---
+++ frontEnd/src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Header from "./component/header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
